36b8dfa5a7480874f5f4ca87f05c3b8ec19811dc
Patrícia Silva <prrs89@gmail.com>
2020-03-18 22:09:59 +0000

    Executing Unit Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-routing');
   });
 
-  it('should render title', () => {
+  it('should render a app-nav in a div tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-routing app is running!');
+    expect(compiled.querySelector('div > app-nav')).toBeDefined();
   });
 });
