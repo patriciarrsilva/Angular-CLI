78129867bbd4b45cc531bca4a1a2ea3fd8e9fb78
Patrícia Silva <prrs89@gmail.com>
2020-03-19 23:04:35 +0000

    Code coverage

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  users: any[];
+  hasPermission = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.hasPermission) {
+      this.getUsers()
+        .then(users => (this.users = this.users))
+        .catch(e => console.log(e.message));
+    } else {
+      this.users = [];
+    }
   }
 
+  async getUsers() {
+    return [
+      { name: 'john', email: 'john@angular.com' },
+      { name: 'collen', email: 'colleen@angular.com' }
+    ];
+  }
 }
